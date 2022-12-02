bb3a74d57eddfcf85f8b0445e09bcaef6fb51556
hjin2 <sunjin980@gmail.com>
2022-12-03 05:44:13 +0900

    프로젝트명 변경

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
   }
   return <div class="flex flex-col justify-center items-center min-h-screen bg-black">
     <img src="division.png" alt="" class="object-scale-down h-48 w-64"/>
-    <h1 className='pt-7 pb-7 text-white font-sans text-6xl'>인기투표</h1>
+    <h1 className='pt-7 pb-7 text-white font-sans text-6xl'>비밀투표</h1>
     <form onSubmit={onSubmit}>
     <select onChange={onChange} className='w-56 h-12 text-center text-2xl font-sans rounded-full'>
       <option value="" className='rounded-full' defaultValue="이름">이름</option>
