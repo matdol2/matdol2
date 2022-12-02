896928bc5810fe37db6c0334a9679f9ced867c5d
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-12-03 05:48:53 +0900

    Update select value

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,29 +1,45 @@
-import { useNavigate } from 'react-router';
+import { useNavigate } from "react-router";
 
 const Login = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const onChange = (e) => {
-    localStorage.setItem('name',e.target.value)
-  }
+    localStorage.setItem("name", e.target.value);
+  };
   const onSubmit = (e) => {
-    e.preventDefault()
-    navigate('/')
-
-  }
-  return <div class="flex flex-col justify-center items-center min-h-screen bg-black">
-    <img src="division.png" alt="" class="object-scale-down h-48 w-64"/>
-    <h1 className='pt-7 pb-7 text-white font-sans text-6xl'>비밀투표</h1>
-    <form onSubmit={onSubmit}>
-    <select onChange={onChange} className='w-56 h-12 text-center text-2xl font-sans rounded-full'>
-      <option value="" className='rounded-full' defaultValue="이름">이름</option>
-      <option value="" className='rounded-full'>김태경</option>
-      <option value="" className='rounded-full'>변찬우</option>
-      <option value="" className='rounded-full'>이상협</option>
-      <option value="" className='rounded-full'>이효진</option>
-    </select>
-    <button className='ml-5 w-20 h-12 text-black bg-white rounded-lg text-lg hover:bg-sky-500 hover:text-white'>로그인</button>
-    </form>
-  </div>;
+    e.preventDefault();
+    navigate("/");
+  };
+  return (
+    <div class="flex flex-col justify-center items-center min-h-screen bg-black">
+      <img src="division.png" alt="" class="object-scale-down h-48 w-64" />
+      <h1 className="pt-7 pb-7 text-white font-sans text-6xl">비밀투표</h1>
+      <form onSubmit={onSubmit}>
+        <select
+          onChange={onChange}
+          className="w-56 h-12 text-center text-2xl font-sans rounded-full"
+        >
+          <option value="" className="rounded-full" defaultValue="이름">
+            이름
+          </option>
+          <option value="김태경" className="rounded-full">
+            김태경
+          </option>
+          <option value="변찬우" className="rounded-full">
+            변찬우
+          </option>
+          <option value="이상협" className="rounded-full">
+            이상협
+          </option>
+          <option value="이효진" className="rounded-full">
+            이효진
+          </option>
+        </select>
+        <button className="ml-5 w-20 h-12 text-black bg-white rounded-lg text-lg hover:bg-sky-500 hover:text-white">
+          로그인
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
