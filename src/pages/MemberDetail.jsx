749ef875aaf5916eaf19e5bd37c1fd21a4459cfa
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-12-03 05:11:08 +0900

    Add vote button

--- src/pages/MemberDetail.jsx
+++ src/pages/MemberDetail.jsx
@@ -2,11 +2,13 @@ import { useEffect, useState } from "react";
 import { RadarChart } from "../components";
 import { api } from "../lib";
 import ReactWordcloud from "react-wordcloud";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const MemberDetail = () => {
   const [data, setData] = useState();
+  const navigate = useNavigate();
   const { name } = useParams();
+
   useEffect(() => {
     (async () => {
       const { data } = await api.post(`/evaluation/search-detail?name=${name}`);
@@ -15,9 +17,19 @@ const MemberDetail = () => {
     })();
   }, []);
 
+  const onClick = () => navigate(`/${name}/insert`);
+
   return (
     <div className="h-screen flex items-center flex-col justify-around">
-      <h1 className="text-8xl">{data?.name}</h1>
+      <div className="text-center space-y-10">
+        <h1 className="text-8xl">{data?.name}</h1>
+        <button
+          onClick={onClick}
+          className="bg-[#28A745] text-white px-8 py-2 rounded-2xl hover:bg-[#21943b] transition"
+        >
+          투표하기기
+        </button>
+      </div>
 
       <div className="flex gap-20">
         <RadarChart
